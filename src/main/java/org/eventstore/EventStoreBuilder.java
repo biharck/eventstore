1c9886475a08c579e6b302ee2fb0e46a2d15a312
Thiago <trbustamante@gmail.com>
2018-08-25 12:39:56 -0300

    Document the public classes

--- src/main/java/org/eventstore/EventStoreBuilder.java
+++ src/main/java/org/eventstore/EventStoreBuilder.java
@@ -3,20 +3,40 @@ package org.eventstore;
 import org.eventstore.message.Publisher;
 import org.eventstore.providers.Provider;
 
+/**
+ * Builder class for {@link EventStore} instances
+ */
 public class EventStoreBuilder {
     private Provider provider;
     private Publisher publisher;
 
+    /**
+     * Inform the {@link Provider} used to handle the persistence of the events in the EventStore
+     * @param provider Handle the event persistence
+     * @return The Builder instance
+     */
     public EventStoreBuilder setProvider(Provider provider) {
         this.provider = provider;
         return this;
     }
 
+    /**
+     * Inform a {@link Publisher} to be used to publish notifications about the modifications in a event stream.
+     * If a Publisher is configured, a {@link org.eventstore.models.Message} will be sent to all
+     * {@link org.eventstore.message.Subscriber}s every time an {@link org.eventstore.models.Event} is added to the
+     * {@link EventStream}
+     * @param publisher Handle the Message notifications
+     * @return The Builder instance
+     */
     public EventStoreBuilder setPublisher(Publisher publisher) {
         this.publisher = publisher;
         return this;
     }
 
+    /**
+     * Create a new {@link EventStore} instance
+     * @return The {@link EventStore} instance
+     */
     public EventStore createEventStore() {
         return new EventStore(provider, publisher);
     }
