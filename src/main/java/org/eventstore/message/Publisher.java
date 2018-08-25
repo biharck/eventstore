63f1f07dcd319fabcd4f210b89533ff67a67807e
Thiago <trbustamante@gmail.com>
2018-08-25 13:24:52 -0300

    Document the public classes

--- src/main/java/org/eventstore/message/Publisher.java
+++ src/main/java/org/eventstore/message/Publisher.java
@@ -2,6 +2,17 @@ package org.eventstore.message;
 
 import org.eventstore.models.Message;
 
+/**
+ * Publish notifications about the modifications in a event stream.
+ * Can send {@link org.eventstore.models.Message} to all
+ * {@link org.eventstore.message.Subscriber}s every time an {@link org.eventstore.models.Event} is added to the
+ * {@link org.eventstore.EventStream}
+ *
+ */
 public interface Publisher {
+    /**
+     * Publish the message to all subscribers
+     * @param message The Message to be published
+     */
     void publish(Message message);
 }
