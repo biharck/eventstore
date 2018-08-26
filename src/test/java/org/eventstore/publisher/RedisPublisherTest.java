12a3fd521290e6268ce193065cf701e044cbf551
Thiago <trbustamante@gmail.com>
2018-08-26 02:42:17 -0300

    Fix test

--- src/test/java/org/eventstore/publisher/RedisPublisherTest.java
+++ src/test/java/org/eventstore/publisher/RedisPublisherTest.java
@@ -48,7 +48,7 @@ public class RedisPublisherTest {
         });
 
         ordersStream.addEvent(new Event(EVENT_PAYLOAD));
-        await().atMost(2, TimeUnit.SECONDS).until(() -> count == 1);
+        await().atMost(5, TimeUnit.SECONDS).until(() -> count == 1);
     }
 
     @Test
@@ -64,7 +64,7 @@ public class RedisPublisherTest {
 
         ordersStream.addEvent(new Event(EVENT_PAYLOAD));
 
-        await().atMost(2, TimeUnit.SECONDS).until(() -> count == 1);
+        await().atMost(5, TimeUnit.SECONDS).until(() -> count == 1);
         subscription.remove();
 
         await().atLeast(1, TimeUnit.SECONDS);
